2310817f184b1576514a3dbcdcc06dfc00606869
KajetanJedrych <jedrychk@gmail.com>
2024-08-01 23:29:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let firstNumber;
+let secondNumber;
+let result;
+
+const plus = (number1, number2) => number1 + number2;
+const minus = (number1, number2) => number1 - number2;
+const multiplication = (number1, number2) => number1 * number2;
+const division = (number1, number2) => number1 / number2;
+console.log(plus(100,100));
+console.log(minus(100,100));
+console.log(multiplication(100,100));
+console.log(division(100,100));
+
+function operate(x, y){
+    plus(x, y);
+}
